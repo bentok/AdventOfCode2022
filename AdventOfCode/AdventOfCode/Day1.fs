a996397ea7ce34a835f258c58c03f5aeed31e4f6
bentok <bscopeland@gmail.com>
2022-12-01 23:37:46 -0500

    Day 1 part 1

---
+++ AdventOfCode/AdventOfCode/Day1.fs
@@ -0,0 +1,41 @@
+﻿module Day1
+
+open System
+open System.IO
+open System.Reflection
+
+let assembly =
+    Assembly.GetExecutingAssembly()
+
+let stream =
+    assembly.GetManifestResourceStream("AdventOfCode.Day1Input.txt")
+    
+let streamToText (input: Stream) =
+    input
+    |> fun input -> new StreamReader(input)
+    |> fun reader -> reader.ReadToEnd()
+
+let calcMostCalories (text: string) =
+    text
+    |> fun str -> str.Split(new string (Environment.NewLine))
+    |> Array.toList
+    |> fun snacks -> ([ [] ], snacks)
+    ||> List.fold (fun acc current ->
+        match current with
+        | snack when snack = "" -> (acc @ [ [] ])
+        | snack ->
+            acc
+            |> List.rev
+            |> fun a ->
+                let head = a |> List.head
+                let tail = a |> List.tail
+                [ (head |> List.append [ snack ]) ] @ tail
+            |> List.rev)
+    |> List.map (List.map int)
+    |> List.map List.sum
+    |> List.max
+
+stream
+|> streamToText
+|> calcMostCalories
+|> printfn "%d"
---
+++ AdventOfCode/AdventOfCodeTests/Tests.fs
@@ -0,0 +1,23 @@
+module Tests
+
+open System
+open System.IO
+open System.Reflection
+open Xunit
+open FsUnit
+open Day1
+
+[<Fact>]
+let ``Day 1 test data`` () =
+    let assembly =
+        Assembly.GetExecutingAssembly()
+
+    let stream =
+        assembly.GetManifestResourceStream("AdventOfCodeTests.Day1TestInput.txt")
+        
+    let expected = 24000
+    let result = calcMostCalories (streamToText stream)
+    
+    printfn "%A" (streamToText stream)
+    
+    result |> should equal expected
